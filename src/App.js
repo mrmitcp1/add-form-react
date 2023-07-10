8f83cd2150f78d961b09fa0366bd196a8442514c
mrmitcp1 <mrmitcp55@gmail.com>
2023-07-10 10:51:24 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [list,setList]=useState([])
+  const [form,setForm]=useState('')
+  const handleChange = (event) =>{
+    setForm(event.target.value)
+  }
+  const handleAddItem = () =>{
+    setList((prevList) => [...prevList, form])
+    setForm('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+    <form>
+      <div className="form-floating">
+        <input className="form-control email" type="text" name="list"  value={form} onChange={handleChange} />
+      </div>
+      <button className="w-100 btn btn-lg btn-primary" type="button" onClick={handleAddItem} >Add</button>
+    </form>
+  <div>
+    <ul>
+      {list.map(item=>
+        <li>{item}</li>
+      )}
+    </ul>
+  </div>
+      </>
   );
 }
 
